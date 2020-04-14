410556c74de31958e0ce7a6f98abb9b6d7e58fbb
jacornelison <james_cornelison@g.harvard.edu>
2020-04-14 18:49:00 -0400

    Initializing package and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='xbmodeling',
+      version='0.1',
+      description='',
+      long_description=readme(),
+      classifiers=[
+        'License :: GNU GPL v3 License',
+        'Programming Language :: Python :: Python 3',
+        'Topic :: Statistics :: Bayesian Analysis',
+      ],
+      url='http://github.com/phys201/extended-beam-modeling',
+      author='',
+      author_email='',
+      license='Gnu GPL v3',
+      packages=['xbmodeling'],
+      install_requires=[
+          '',
+      ],
+      zip_safe=False)
+
