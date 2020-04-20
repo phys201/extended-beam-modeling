6505db5d39bae7d08c61838513c60642c852ac1a
James Cornelison <james_cornelison@g.harvard.edu>
2020-04-20 19:18:54 -0400

    Added the GenMapModel class and improved a bunch of functionality

--- xbmodeling/config.py
+++ xbmodeling/config.py
@@ -3,15 +3,15 @@
 # shop so we don't have to make a bunch of changes
 # in the code if we want to change these values
 
-#import io
+# import io
 
 # Values for the generative model
 modelconf = {
-    "cmbFile" : "inputmaps/COM_CMB_IQU-143-fgsub-sevem-field-Pol_1024_R2.01_full.fits", # Default CMB map
-    "groundTemperature" : 300, # Default ground temperature
-    "beamParams" : [1, 0, 30, 30, 30, 1],
-    "extendedBeam" : [0.25, 120, 120, 60, 60, 0]
-
+    "cmbFile": "xbmodeling/inputmaps/COM_CMB_IQU-143-fgsub-sevem-field-Pol_1024_R2.01_full.fits",  # Default CMB map
+    "groundTemperature": 300,  # Default ground temperature
+    "beamParams": [1, 0, 30, 30, 30, 1],
+    "extendedBeam": [0.25, 120, 120, 60, 60, 0],
+    "defaultResolution": 128,
 }
 
-#"BeamParams" : io.load_beam_params()
+# "BeamParams" : io.load_beam_params()
